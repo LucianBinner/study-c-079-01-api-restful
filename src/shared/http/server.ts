185aac532173c7d65349a9fe5d25cfbc8dd8f3b3
Lucian Binner <lucian.binner@hotmail.com>
2022-07-16 17:38:07 -0300

    feat: customizing errors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,29 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes/index';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  // Middleware que intercepta todos os erros gerados na aplicação
+  if (error instanceof AppError) {
+    // Verifica se o erro gerado é uma intancia de AppError
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    // Se a o erro não for uma intancia de AppError é gerado um server error genérico
+    status: 'error',
+    message: 'Internal server error!',
+  });
+});
 
 app.listen(3333, () => {
   console.log('Server stater on port 3333!');
